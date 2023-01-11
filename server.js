f390e3e98eb66b5240713186707a8c0cf98736e2
HK-9 <37357889+HK-9@users.noreply.github.com>
2023-01-11 18:54:02 +0530

    cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config({path:'./.env'})
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 const cookieParser   =  require('cookie-parser');
 const dbconnection = require('./db')
@@ -9,8 +10,10 @@ const authRoute = require('./routes/authRoute');
 const varifyJWT = require('./middleware/varifyJWT');
 // require('dotenv').config();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors());
 
 app.use('/api/auth', authRoute);
 app.use('/api/cars',varifyJWT,usersRoute);
