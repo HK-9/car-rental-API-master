e05ebd8419912e5b644122c7ef9d552377d0f9fd
HK-9 <37357889+HK-9@users.noreply.github.com>
2023-01-19 17:35:35 +0530

    bookings api fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -7,9 +7,9 @@ const stripe = require("stripe")(
 
 exports.getallbookings = async (req, res) => {
   try {
-    console.log('getallbookings/req.body.decoded:',req.body.decoded,"body::",req.body)
-    if(!req.body.decoded) return res.status(401).json({message:'unothorized:Login again'})
-    const userId = req.body.decoded.id;
+    console.log('getallbookings/req.body.decoded:',req.query.user,"body::",req.body)
+    if(!req.query.user) return res.status(401).json({message:'unothorized:Login again'})
+    const userId = req.query.user;
     const bookings = await Booking.find({user:userId}).populate("car").lean();
     res.status(200).send(bookings);
   } catch (error) {
