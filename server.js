2f47f92d3e036ffb9b7f96b8b85f857b3583fd5d
HK-9 <37357889+HK-9@users.noreply.github.com>
2023-01-19 13:27:44 +0530

    jwt middleware removed2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://www.rentx.gamexonline.store");
+    res.header("Access-Control-Allow-Origin", "");
     res.header("Access-Control-Allow-Credentials", "true");
     next();
   });
@@ -24,7 +24,7 @@ app.use(function(req, res, next) {
 app.use(cors({ origin:true, credentials:true}));
 
 app.use('/api/auth', authRoute);
-app.use('/api/cars',varifyJWT,usersRoute);
+app.use('/api/cars',usersRoute);
 app.use('/api/bookings',usersRoute);
 app.use('/api/admin',varifyJWT,adminRoute)
 ///add api version;
