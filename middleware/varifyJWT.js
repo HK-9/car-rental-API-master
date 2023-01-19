4e550cd1da7567bcffbe93f3d842f4823d9a3a17
HK-9 <37357889+HK-9@users.noreply.github.com>
2023-01-19 16:33:44 +0530

    jwt cookie fix

--- middleware/varifyJWT.js
+++ middleware/varifyJWT.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 const varifyJWT = (req,res,next)=>{
     const cookie = req.headers.cookie;
     if(!cookie) return next();
-    const jwtToken = cookie.split("=")[1];
+    const xjwtToken = cookie.split("=")[1];
+    const jwtToken = xjwtToken.split(";")[0];
     if(!jwtToken) return res.status(402).json({message:'unauthorized'});
     jwt.verify(jwtToken,process.env.JWT_SECRET,(err,decoded)=>{
         if(err){
