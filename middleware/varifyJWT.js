2289f1ffd77ccf51c80a48fe2b2d8ac4dffca739
HK-9 <37357889+HK-9@users.noreply.github.com>
2023-01-20 15:43:51 +0530

    error fix

--- middleware/varifyJWT.js
+++ middleware/varifyJWT.js
@@ -4,21 +4,20 @@ const varifyJWT = (req,res,next)=>{
     try {
         
         const cookie = req.headers.cookie;
-        if(!cookie) return res.status(401).joson({message:'no cookie found in header'});
+        if(!cookie) return next();
         const xjwtToken = cookie.split("=")[1];
-        if(!xjwtToken) return res.status(402).json({message:'unauthorized, jwt missing'});
+        if(!xjwtToken) return res.status(402).json({message:'unauthorized'});
         const jwtToken = xjwtToken.split(";")[0];
         jwt.verify(jwtToken,process.env.JWT_SECRET,(err,decoded)=>{
             if(err){
                 return res.status(403).json({message:'Forbiden-manupulated token'});
             }
-            req.userId = decoded.id;
-            console.log("decoded",req.userId)
+            req.user = decoded
             next();
         })
     } catch (error) {
         console.log(error)
-        res.status(400).json({status:'jwt middleware failed',message:error})
+        res.status(400).json({message:"error"})
     }
 }
 
