1c08ee821b6b7d743012a5a984686736d029376f
HK-9 <37357889+HK-9@users.noreply.github.com>
2023-01-30 16:34:34 +0530

    deployment fixes - otp,route,domain-utl

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'https://hk-9-car-rental-client2.vercel.app',
     'https://hk-9-car-rental-client2-pq2txd7er-hk-9.vercel.app',
-    'https://master.d25m9x70tbafqp.amplifyapp.com',
+    'https://master.d1hl1zb9wkb98b.amplifyapp.com ', //mumbai 
+    'https://master.d25m9x70tbafqp.amplifyapp.com', //tokyo
     'http://127.0.0.1:3000',
     'http://localhost:3000',
     'http://localhost:3000',
